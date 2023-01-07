daddda49ceaf663fb24a5eba33b35b19c9aa91ec
Mael <mael.thomas@data.gouv.fr>
2023-01-07 18:29:38 +0100

    On enlève l'image og:image ed base

--- components/meta.js
+++ components/meta.js
@@ -36,7 +36,6 @@ export default function Meta() {
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   )
 }
